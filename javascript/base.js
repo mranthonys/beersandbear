b22b49b2ecc2820252564c59e0b87af59a6edcb2
Chloe Burrell <chloeburrell@chloes-mbp.home>
2021-03-02 10:20:02 -0500

    first commit

---
+++ javascript/base.js
@@ -0,0 +1,67 @@
+
+
+// function toggleMenu() {
+
+//     var x = document.getElementById("myMenu");
+//         if (x.style.display = "none") {
+//             x.style.display = "block";
+//         } else {
+//             x.style.display = "none";
+//         }
+
+// }
+
+function toggleMenu(myMenu) {
+    var x = document.getElementById("myMenu");
+    console.log(x.style.display)
+    if (x.style.display !== 'none') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = '';
+    }
+    call(myMenu); 
+}
+
+const mainNav = document.querySelector('.main-nav')
+
+// function toggeMenu()
+//     document.getElementById.toggle('myMenu')
+// function toggleMenu() 
+function toggleMainNav(){
+    
+        // function toggleMenu(myMenu) {
+        //     var x = document.getElementById("myMenu");
+        //     console.log(x.style.display)
+        //     if (x.style.display !== 'none') {
+        //         x.style.display = 'none';
+        //     } else {
+        //         x.style.display = '';
+        //     }
+        //     call(myMenu); 
+        // }
+    mainNav.classList.toggle('active')
+}
+
+mainNav.addEventListener('click', toggleMainNav);
+
+
+// myMenu.addEventListener('click', toggleMenu);
+
+
+
+function changeImage (target, img1, img2) {
+
+    if (document.getElementById(target).src == img1)
+    {
+
+        document.getElementById(target).src = img2;
+
+    }
+
+    else{
+
+        document.getElementById(target).src = img1;
+
+    }
+
+}
